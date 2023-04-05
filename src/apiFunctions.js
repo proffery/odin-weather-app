7118a1994845ad199c8dbe3625ef4ac2b8ba53d7
Dmitry Shamko <proffery@gmail.com>
2023-04-05 17:24:56 +0300

    api functions work

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,27 +1,57 @@
-const WEATHER_API_KEY = '3edcd2b2225748b2a0c84115230304'
-const LOCATION_BY_OP_API_KEY = 'bdc_d421cee4e72e4bba8e17ae387919f89e'
-
-async function getWeather(city) {
-  try {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`, {mode: 'cors'});
-    const weatherData = await response.json();
-    //console.log(weatherData);
-
-  } catch(err) {
-    console.log(err);
+const weather = (() => {
+  const WEATHER_API_KEY = '3edcd2b2225748b2a0c84115230304'
+  const LOCATION_BY_OP_API_KEY = 'bdc_d421cee4e72e4bba8e17ae387919f89e'
+  
+  async function getWeather(city) {
+    try {
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`, {mode: 'cors'});
+      const weatherObj = await response.json();
+      let cleanWeatherObj = await getData(weatherObj);
+      //console.log(cleanWeatherObj);
+      return cleanWeatherObj;
+  
+    } catch(err) {
+      console.log(err.message);
+      const location = await getLocation()
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location}`, {mode: 'cors'});
+      const weatherObj = await response.json();
+      let cleanWeatherObj = await getData(weatherObj);
+      //console.log(cleanWeatherObj);
+      return cleanWeatherObj
+    }
   }
-}
-
-async function getLocation() {
-  try {
-    const response = await fetch(`https://api.bigdatacloud.net/data/ip-geolocation?key=${LOCATION_BY_OP_API_KEY}`, {mode: 'cors'});
-    const locationData = await response.json();
-    //console.log(locationData.location.city);
-    return locationData.location.city;
-
-  } catch(err) {
-    console.log(err);
+  
+  async function getLocation() {
+    try {
+      const response = await fetch(`https://api.bigdatacloud.net/data/ip-geolocation?key=${LOCATION_BY_OP_API_KEY}`, {mode: 'cors'});
+      const locationData = await response.json();
+      //console.log(locationData.location.city);
+      return locationData.location.city;
+  
+    } catch(err) {
+      console.log(err.message);
+    }
   }
-}
+  
+  function getData(data) {
+    const tempObj = JSON.stringify({
+      city: data.location.name,
+      country: data.location.country,
+      temp_c: data.current.temp_c,
+      temp_f: data.current.temp_f,
+      feelslike_c: data.current.feelslike_c,
+      feelslike_f: data.current.feelslike_f,
+      condition: data.current.condition.text,
+      icon: data.current.condition.icon,
+      wind_kph: data.current.wind_kph,
+      wind_mph: data.current.wind_mph
+    });
 
-export {getWeather, getLocation};
+    return JSON.parse(tempObj)
+  }
+
+  return {getWeather};
+
+})();
+
+export {weather};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import './style.css'
 import logoImg from './img/106169566.jpg'
-import { getWeather, getLocation } from './apiFunctions';
+import {weather} from './apiFunctions.js'
 
-console.log(getWeather(`${getLocation()}`));
-//getLocation();
+const container = document.querySelector('.container');
+const city = document.querySelector('.city');
+const icon = document.querySelector('.icon');
+const temp = document.querySelector('.temp');
+const country = document.querySelector('.country');
+
+
+function setWeather(cityName) {
+    weather.getWeather(cityName).then((value) => {
+        country.textContent = value.country + ',';
+        city.textContent = value.city + ':';
+        icon.src = value.icon;
+        icon.title = value.condition;
+        temp.textContent = value.temp_c + '°C';
+        console.log(value.city);
+    });
+}
+
+setWeather('london');
